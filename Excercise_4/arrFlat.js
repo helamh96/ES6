d18a966d42a41c6a62a0ddfc52092ab0c7d0023e
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-06 13:27:20 -0500

    Add files via upload

--- Excercise_4/arrFlat.js
+++ Excercise_4/arrFlat.js
@@ -1,19 +1,18 @@
-let btn = document.getElementById("btn")
-btn.addEventListener("click",flat)
+const arr = [1,2,7,[3,[4,5]],4,5,[3,4]]
 
-function flat(){
-    let array = JSON.parse(document.getElementById("array").value)
-    function hasObject(array){
-        for (let arr of array){
-            if (typeof arr === "object" && arr){
-                return true
-            }
-        }
+function flat(arr) {
+    let result = [] 
+    let main = arr, first
+    while(main.length > 0) { 
+       first = main[0]
+       if(Array.isArray(first)) { 
+       Array.prototype.splice.apply(main, [0, 1].concat(first))
+    } else {
+       result.push(first)
+       main.splice(0, 1)
     }
-
-    while (hasObject(array)){
-        array = array.flat()
     }
-  
-    alert(`Flaten array: [${array}]`)
+    return result
 }
+
+console.log(flat(arr))
