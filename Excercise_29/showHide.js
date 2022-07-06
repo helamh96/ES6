d3cf29267c257c6bc87186b0bfbd11df244b68a7
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-06 13:42:06 -0500

    Add files via upload

--- Excercise_29/showHide.js
+++ Excercise_29/showHide.js
@@ -1,15 +1,16 @@
 let id1 = document.getElementById("id1")
 let div = document.querySelector("div")
 
+console.log(div)
 
 id1.addEventListener("click", showHide)
 
 function showHide(){
-    if (div.style.visibility === "hidden"){
-        div.style.visibility = "visible"
-        id1.textContent = "Hide"
-    }else{
+    if (div.style.visibility === "visible"){
         div.style.visibility = "hidden"
         id1.textContent = "Show"
+    }else{
+        div.style.visibility = "visible"
+        id1.textContent = "Hide"
     }
 }
