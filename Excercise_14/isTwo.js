6cafcff8a00076e9c400dc0dac62c2e156a68b84
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-06 13:33:04 -0500

    Add files via upload

--- Excercise_14/isTwo.js
+++ Excercise_14/isTwo.js
@@ -6,21 +6,10 @@ function checkText(){
     let strValues = document.getElementById("parameter").value
     let strArray = strValues.split(",")
     let numValues = strArray.map(n=> parseInt(n,10))
-    let check = includesTwo(...numValues)
+    let check = numValues.includes(2)
     if (check){
         alert("The function includes 2 as aparemeter.")
     }else{
         alert("The function does not include 2 as a paremeter.")
-    }
-}
-
-
-function includesTwo(){
-    let parameters = Object.values(arguments)
-    if (parameters.includes(2)){
-        return true
-    }
-    else{
-        return false
     }
 }
