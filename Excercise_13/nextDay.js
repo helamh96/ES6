ed1effb9508efb974184fd9ba78833b11797e71a
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-06 13:31:39 -0500

    Add files via upload

--- Excercise_13/nextDay.js
+++ Excercise_13/nextDay.js
@@ -2,10 +2,10 @@ Date.prototype.nextDay = nextDay
 
 function nextDay(){
     let current = this.getDate()
-    let newDate = new Date(this.setDate(current))
+    let newDate = new Date
     newDate.setDate(current+1)
     return newDate
 }
 
 let date = new Date() 
-console.log("Today is "+date+", and tomorrow will be "+date.nextDay())
+console.log(`Today is ${date}, and tomorrow will be ${date.nextDay()}`)
