fbd5952f4c0eca4aa144980d788af7cf39ac494a
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-06 13:45:05 -0500

    Add files via upload

--- Excercise_40/sumZero.js
+++ Excercise_40/sumZero.js
@@ -1,7 +1,10 @@
-let arr = [0,1,-4,-2,-9,4,6,2,7]
+// let arr = [0,1,-4,-2,-9,4,6,2,7]
+
+let arr = [3,2,1,0,-3,2,2,-2]
 
 function findNumbers(array){
     let sums0 = [];
+    array = [...new Set(array)]
     for (let i = 0; i < array.length-2; i++){
         for (let j= i+1; j < array.length-1; j++){
             for (let k = j+1; k<array.length; k++){
