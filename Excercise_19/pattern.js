703c8b595c5f326eae9de0f508f922c9a5899274
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-27 16:57:58 -0500

    Add files via upload

--- Excercise_19/pattern.js
+++ Excercise_19/pattern.js
@@ -1,27 +1,26 @@
-const singleChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z",1,2,3,4,5,6,7,8,9,0]
 let btn = document.getElementById("btn")
 btn.addEventListener("click",searchPattern)
 
 function searchPattern(){
     let p = document.getElementById("pattern").value
     let text = document.querySelector(".text").textContent
-    console.log(p)
-    if(p.includes("*")){
-        for(let i=0; i<singleChar.length; i++){
-            let newp = p.replace("*",singleChar[i])
-            search(text,newp)
-        }
-    }else{
-        search(text,p)
-    }
-    
+    search(text,p)
 }
 
 function search(text,p){
     const arr = text.split(" ")
     for(let a of arr){
-        if(a.match(p) && a.length === p.length){
-            console.log("string "+a+" matches")
+        let cont = 0
+        for(let j=0; j<p.length; j++){
+            if(p.charAt(j)!="*"){
+                if(a.charAt(j)==p.charAt(j)){
+                    cont++
+                }
+            }else{cont++}
         }
+        if(cont==a.length && cont==p.length ){
+            console.log(`The string "${a}" matches with the pattern`)
+        }    
     }
+    
 }
