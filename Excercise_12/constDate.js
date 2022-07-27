526ab6e5f780a20c156b9f307440c1ac23ed2910
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-27 17:32:38 -0500

    Add files via upload

--- Excercise_12/constDate.js
+++ Excercise_12/constDate.js
@@ -1,14 +1,24 @@
-function modifyDateObject(name,fun){
-    Date.prototype[name] = fun;
-}
-
-function customDate(){
-    return "April 1st";
-}
-
-modifyDateObject("constantDate", customDate);
-
 let date = new Date()
-date.setUTCHours(15,31,01);
 
-console.log(date);
+function changeDate(date,format){
+    if(format=="milliseconds"){
+        let ms = date.getMilliseconds()
+        return `Since this date have passed ${ms} milliseconds`
+    }else if(format=="am/pm"){
+        let hours = date.getHours()
+        let minutes = date.getMinutes()
+        let ampm = hours >= 12 ? "pm" : "am"
+        hours = hours % 12
+        hours = hours ? hours : 12
+        minutes = minutes < 10 ? "0"+minutes : minutes
+        let strTime = hours + ":" + minutes + " " + ampm
+        return strTime
+    }else if(format == "dayOfWeek"){
+        let day = date.getDay()
+        const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+        day = weekday[day]
+        return `the day is ${day}`
+    }
+}
+
+console.log(changeDate(date,"dayOfWeek"))
