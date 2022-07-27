b0a99b44bdcd94f6e4d499f3b876154e22604f8c
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-27 16:53:50 -0500

    Add files via upload

--- Excercise_18/candidatesTable.js
+++ Excercise_18/candidatesTable.js
@@ -7,6 +7,8 @@ let phoneCont = document.getElementById('phone')
 let inputArea = document.querySelector('.inputs')
 let modifyArea = document.querySelector('.modify')
 let changeButton = document.getElementById('change-button')
+const deleteButton = document.getElementById("delete-button")
+const cancelButton = document.getElementById("cancel-button")
 
 var fakeData = {
     1:{name: {
@@ -27,52 +29,88 @@ function tableClicked(event){
     let clicked = event.target
     let cId = clicked.getAttribute('id')
     console.log(cId)
+    
+
     if(cId==="adding"){
         inputArea.style.display = "block"
         table.style.display = 'none'
         modifyArea.style.display = 'none'
-    }else{
+        addButton.style.display = "block"
+        cancelButton.style.display = "block"
+        cancelButton.addEventListener("click",cancelEdit)
+    }else if(cId!=null){
         let modifyName = document.getElementById('modify-name')
         let modifyLastName = document.getElementById('modify-last-name')
         let modifyPhone = document.getElementById('modify-phone')
+        modifyName.value = fakeData[cId].name.firstname
+        modifyLastName.value = fakeData[cId].name.lastname
+        modifyPhone.value = fakeData[cId].name.phone
         inputArea.style.display = 'none'
         table.style.display = 'none'
         modifyArea.style.display = 'block'
-        changeButton.addEventListener('click', saveChanges, {'once':true})
-        fetch('#').then( setData(cId) )
+        deleteButton.style.display = "block"
+        cancelButton.style.display = "block"
+        changeButton.style.display = "block"
+        deleteButton.addEventListener("click", deleteUser,{once: true})
+        changeButton.addEventListener('click', saveChanges, {once: true})
+    }   
 
-        function saveChanges(){
-            let newName = modifyName.value
-            let newLastName = modifyLastName.value
-            let newPhone = modifyPhone.value
-            if (!newName && !newLastName && !newPhone){
-                fetch('#',{method:"DELETE"}).then(()=> {delete fakeData[cId]
-                                                        fillInTable(fakeData)})
-            }else{
-                let newCandidate = {name:{
-                    firstname:newName,
-                    lastname: newLastName,
-                    phone: newPhone
-                    }}
-                fetch('#', {
-                    method: 'POST',
-                    body: newCandidate })
-                           .then(()=>{
-                               fakeData[cId] = newCandidate
-                               fillInTable(fakeData)
-                           })
-            }
-            inputArea.style.display = 'none'
-            table.style.display = 'inline-block'
-            modifyArea.style.display = 'none'
-        }
-    
-        function setData(cId){
-            modifyName.value = fakeData[cId].name.firstname
-            modifyLastName.value = fakeData[cId].name.lastname
-            modifyPhone.value = fakeData[cId].name.phone
-        }
+    function saveChanges(){
+        let modifyName = document.getElementById('modify-name')
+        let modifyLastName = document.getElementById('modify-last-name')
+        let modifyPhone = document.getElementById('modify-phone')
+        let newName = modifyName.value
+        let newLastName = modifyLastName.value
+        let newPhone = modifyPhone.value
+        let newCandidate = {name:{
+            firstname:newName,
+            lastname: newLastName,
+            phone: newPhone
+            }}
+        fetch('#', {method:'PATCH',body:JSON.stringify(newCandidate) })
+        .then(()=>{
+            fakeData[cId] = newCandidate
+            fillInTable(fakeData)
+            })
+        console.log(fakeData)
+
+        nameCont.value = ''
+        lastNameCont.value = ''
+        phoneCont.value = ''
+        inputArea.style.display = 'none'
+        table.style.display = 'inline-block'
+        modifyArea.style.display = 'none'
+        changeButton.style.display = "none"
+        deleteButton.style.display = "none"
+        cancelButton.style.display = "none"
     }
+
+    function deleteUser(){
+        nameCont.value = ''
+        lastNameCont.value = ''
+        phoneCont.value = ''
+        fetch('#',{method:"DELETE"}).then(()=> {delete fakeData[cId]
+            fillInTable(fakeData)})
+        inputArea.style.display = 'none'
+        table.style.display = 'inline-block'
+        modifyArea.style.display = 'none'
+        changeButton.style.display = "none"
+        deleteButton.style.display = "none"
+        cancelButton.style.display = "none"
+    }
+
+    function cancelEdit(){
+        nameCont.value = ''
+        lastNameCont.value = ''
+        phoneCont.value = ''
+        inputArea.style.display = 'none'
+        table.style.display = 'inline-block'
+        modifyArea.style.display = 'none'
+        addButton.style.display = "none"
+        changeButton.style.display = "none"
+        cancelButton.style.display = "none"
+        deleteButton.style.display = "none"
+    } 
 }
 
 
@@ -82,7 +120,7 @@ function addCandidate(){
     let textLastName = lastNameCont.value
     let textPhone = phoneCont.value
 
-    if (textName && textLastName && textPhone){
+    if (textName || textLastName || textPhone){
         let sendData = {name:
                             {
                                 firstname:textName,
@@ -92,12 +130,10 @@ function addCandidate(){
                         }
         fetch('#', {
                 method: 'POST',
-                body: sendData })
-                       .then(response => {  let strIndices = Object.keys(fakeData)
-                                            let intIndices = strIndices.map(i=>parseInt(i))
-                                            let nextIndex = Math.max(intIndices) +1
-                                            let strNextInd = String(nextIndex)
-                                            fakeData[strNextInd] = sendData
+                body: JSON.stringify(sendData) })
+                       .then(response => {  
+                                            let id = Date.now()
+                                            fakeData[id] = sendData
                                             fillInTable(fakeData)
                                             })
     
@@ -108,6 +144,8 @@ function addCandidate(){
         table.style.display = 'inline-block'
         modifyArea.style.display = 'none'
     }
+    addButton.style.display = "none"
+    cancelButton.style.display = "none"
 }
 
 
