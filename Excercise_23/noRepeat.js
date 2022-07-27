86ff53ef51f6915c8f4bc2a5219c3e970abd6928
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-07-27 17:12:25 -0500

    Add files via upload

--- Excercise_23/noRepeat.js
+++ Excercise_23/noRepeat.js
@@ -1,57 +1,28 @@
 let genButton = document.getElementById("genera")
-let minBox = document.getElementById("min")
-let maxBox = document.getElementById("max")
-let nextButton = document.getElementById("next")
 let textArea = document.querySelector(".text-area")
 
 genButton.addEventListener("click", startShowing)
 
-function startShowing(){
-    let min = minBox.value
-    let max = maxBox.value
-    genButton.style.display = "none"
-    nextButton.style.display = "inline-block"
-    nextButton.value = "Next"
-    maxBox.disabled = true
-    minBox.disabled = true
-
-    let rand = getRandom(min,max)
-    applyNext()
-    nextButton.addEventListener("click", applyNext)
-
-    function applyNext(){
-        if(textArea.textContent.includes("printed")){
-            nextButton.removeEventListener("click", applyNext)
-            genButton.style.display = "inline-block"
-            nextButton.style.display = "none"
-            maxBox.disabled = false
-            minBox.disabled = false
-            textArea.textContent = ""
-        }else{
-            textArea.textContent=rand()
-        }
-        if(textArea.textContent.includes("printed")){
-            nextButton.value = "Done!"
-        }
+function* random() {
+    let min = document.getElementById("min").value
+    let max = document.getElementById("max").value
+    let i = max - min +1
+    let arr = Array.from(Array(i).keys()).map((idx) => Number(min) + Number(idx))
+    while (i--){
+        yield arr.splice(Math.floor(Math.random()*(i+1)),1)[0]
     }
+}
 
+let output = random()
 
-    function getRandom(min,max){
-        let allNumbers = []
-        for (let n = min; n<=max; n++){
-            allNumbers.push(n)
-        }
-        
-        function getNumber(){
-            let range = allNumbers.length
-            if (range <= 0){
-                return `All numbers in the interval [${min}, ${max}] have been printed`
-            }
-            let index = Math.floor(Math.random()*range)
-            let randomResult = allNumbers[index]
-            allNumbers.splice(index, 1)
-            return randomResult
-        }
-        return getNumber
+function startShowing(){
+    genButton.value = "next"
+    let nextV = output.next().value
+    if(nextV!=undefined){
+        textArea.textContent = nextV
+    }else{
+        textArea.textContent = "All numbers have been displayed, click generate for start again"
+        genButton.value = "Generate"
+        output = random()
     }
 }
