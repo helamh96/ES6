6d68a9e87de3c3d823571af948b5543a9f033074
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-08-08 20:25:54 -0500

    Update constDate.js

--- Excercise_12/constDate.js
+++ Excercise_12/constDate.js
@@ -1,24 +1,11 @@
-let date = new Date()
+function MyDate(year,month,day) {
+    let date = new Date();
+    date.setFullYear(year);
+    date.setMonth(month);
+    date.setDate(day);
+    return date;
+}  
 
-function changeDate(date,format){
-    if(format=="milliseconds"){
-        let ms = date.getMilliseconds()
-        return `Since this date have passed ${ms} milliseconds`
-    }else if(format=="am/pm"){
-        let hours = date.getHours()
-        let minutes = date.getMinutes()
-        let ampm = hours >= 12 ? "pm" : "am"
-        hours = hours % 12
-        hours = hours ? hours : 12
-        minutes = minutes < 10 ? "0"+minutes : minutes
-        let strTime = hours + ":" + minutes + " " + ampm
-        return strTime
-    }else if(format == "dayOfWeek"){
-        let day = date.getDay()
-        const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-        day = weekday[day]
-        return `the day is ${day}`
-    }
-}
-
-console.log(changeDate(date,"dayOfWeek"))
+const myDate = new MyDate(1996,3,6);
+  
+console.log(myDate);
