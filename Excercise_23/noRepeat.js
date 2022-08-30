b96b41f1e4c605d0bf44190ea6207c2fdd885753
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-08-30 18:30:31 -0500

    Update noRepeat.js

--- Excercise_23/noRepeat.js
+++ Excercise_23/noRepeat.js
@@ -1,57 +1,42 @@
 const textArea = document.getElementById("text-area")
 const genButton = document.getElementById("genera")
-
+genButton.addEventListener("click",setValue,{once:true})
 genButton.addEventListener("click", startShowing)
 
-min=-2
-max=5
+let max, min, i, count, a, c, s
 
-function primeFactors(n) {
-    const factors = [1];
-    let divisor = 2;
-  
-    while (n >= 2) {
-      if (n % divisor == 0) {
-        factors.push(divisor);
-        n = n / divisor;
-      } else {
-        divisor++;
-      }
+function setValue(){
+    max = Number(document.getElementById("max").value)
+    min = Number(document.getElementById("min").value)
+    i = max - min + 1 
+    count = 0
+    m = Math.ceil(Math.log2(i))
+    if(m<4){
+        a = 1
+    }else{
+        a = 1 + Math.floor(4*m)
     }
-    return factors;
+    c = 3**(Math.floor(m/2))
+    s = Math.floor(Math.random()*(i-2)+1)
 }
 
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
-}
 
-let count = 0
-let i = max-min+1
-let div = 1
-let pf = primeFactors(i)
-let dif = pf.filter(onlyUnique)
-dif.forEach(e => div=div*e)
-if(i%4==0){
-    div*=4
-}
-if(dif.includes(2)){
-    div=div/2
-}
-div+=1
-let x = Math.floor(Math.random()*i) + min
-let xj = x
-
-function applyRand(xj){
-    let xi = (div*xj+5)%i
-    return xi
+function applyRand(seed){
+    let xi = (a*seed + c)%(2**m)
+    if(xi<i){
+        s = xi
+        count++
+    }
+    else{
+        applyRand(xi)
+    }
 }
 
 
 function startShowing(){
-    count++
     if(count!=i){
-        xj= applyRand(xj)
-        textArea.innerText = xj+min
+        applyRand(s)
+        textArea.innerText = s + min
     }else{
         textArea.innerText = "All Elements have been displayed"
         genButton.disabled = true
