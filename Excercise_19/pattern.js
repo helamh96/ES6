1f03fc8f6ac939cd833adc0744856ffe901a5ce6
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-09-12 13:18:51 -0500

    Update pattern.js

--- Excercise_19/pattern.js
+++ Excercise_19/pattern.js
@@ -4,10 +4,11 @@ btn.addEventListener("click",searchPattern)
 function searchPattern(){
     let p = document.getElementById("pattern").value
     let text = document.querySelector(".text").textContent
-    console.log(findMatchingText(text,p))
+    findMatchingText(text,p)
 }
 
 function findMatchingText(text, p){
+    let matches = []
     const firstString = p.replace(/\*/g,"")[0]
     if(p[0] != firstString){
         var newVal = p.indexOf(firstString)
@@ -17,36 +18,36 @@ function findMatchingText(text, p){
     if(text.indexOf(firstString)< newVal  || text.indexOf(firstString)== -1){
         return "there are no matches"
     }else{
+        let count =1;
+        let word = p[newVal]
         let indices = [];
         for(let i=0; i<text.length;i++) {
             if (text[i] === p[newVal]) indices.push(i);
         }
-        console.log(indices)
-        let count =-1;
+        
         indices.forEach(e => {
             let index = Number(e)
-            for(let i=0; i<newVal+1; i++){
+            for(let i=1; i<newVal+1; i++){
                 if(text[index-i]==p[newVal-i] || p[newVal-i]=="*"){
                     count++
+                    word = text[index-i] + word
                 }
             }
-            for(let i=0; i<p.length-newVal; i++){
+            for(let i=1; i<p.length-newVal; i++){
                 if(text[index+i]==p[newVal+i] || p[newVal+i]=="*"){
                     count++
+                    word = word + text[index+i] 
                 }
             }
             if(count === p.length){
-                return true
+                matches.push(word)
+                word = p[newVal]
+                count = 1
             }else{
-                count = -1;
-                return true
+                word = p[newVal]
+                count = 1;
             }
         });
-        
-        if(count == p.length){
-            return true
-        }else{
-            return false
-        }
     }
+    return matches
 }
