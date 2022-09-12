783fc4f7601bf0ed82369c42b373ea2cd7dbe137
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-09-12 18:17:05 -0500

    Update noRepeat.js

--- Excercise_23/noRepeat.js
+++ Excercise_23/noRepeat.js
@@ -1,44 +1,77 @@
 const textArea = document.getElementById("text-area")
-const genButton = document.getElementById("genera")
-genButton.addEventListener("click",setValue,{once:true})
-genButton.addEventListener("click", startShowing)
+const genButton = document.getElementById("generate")
+const restartButton = document.getElementById("restart")
 
-let max, min, i, count, a, c, s
+restartButton.addEventListener("click",restart)
+genButton.addEventListener("click", setVal,{once:true})
+genButton.addEventListener("click", show)
+let num 
 
-function setValue(){
-    max = Number(document.getElementById("max").value)
-    min = Number(document.getElementById("min").value)
-    i = max - min + 1 
-    count = 0
-    m = Math.ceil(Math.log2(i))
-    if(m<4){
-        a = 1
-    }else{
-        a = 1 + Math.floor(4*m)
+class Random{
+    constructor(max,min,s,a,c,m,i){
+        this.max = max
+        this.min = min
+        this.s = s
+        this.a = a
+        this.c = c
+        this.m = m
+        this.i = i
     }
-    c = 3**(Math.floor(m/2))
-    s = Math.floor(Math.random()*(i-2)+1)
+
+    *random(s,a,c,m,i){
+        let count = 0
+        let ranVals = []
+        while(count != i){
+            let xi = (a*s + c)%(2**m)
+            s = xi
+            if(xi<i){
+                ranVals.push(xi)
+                count++
+            }
+        }
+        count = 0
+        while(count != this.i){ 
+            yield ranVals[count] + this.min
+            count ++
+        }
+    }
+}
+
+function setVal(){
+    let max = Number(document.getElementById("max").value)
+    let min = Number(document.getElementById("min").value)
+    let i = max - min + 1 
+    let m = Math.ceil(Math.log2(i))
+    if(m<=4){
+        var a = 1
+    }else{
+        var a = 1 + Math.floor(4*m)
+    }
+    let c = 3**(Math.floor(m/2))
+    let s = Math.floor(Math.random()*(i-2)+1)
+    num = new Random(max,min,s,a,c,m,i)
+    num = num.random(s,a,c,m,i)
+}
+
+function show(){
+    let next = num.next().value
+    if(next !== undefined){
+        textArea.innerText = next
+    }else{
+        count = 0
+        textArea.innerText = "All numbers have been displayed"
+        genButton.style.display = "none"
+        restartButton.style.display = "inline"
+    }
 }
 
 
-function applyRand(seed){
-    let xi = (a*seed + c)%(2**m)
-    if(xi<i){
-        s = xi
-        count++
-    }
-    else{
-        applyRand(xi)
-    }
-}
-
-
-function startShowing(){
-    if(count!=i){
-        applyRand(s)
-        textArea.innerText = s + min
-    }else{
-        textArea.innerText = "All Elements have been displayed"
-        genButton.disabled = true
-    }
+function restart(){
+    genButton.removeEventListener("click", setVal)
+    genButton.removeEventListener("click", show)
+    textArea.innerText = ""
+    genButton.style.display = "inline"
+    restartButton.style.display = "none"
+    genButton.addEventListener("click", setVal,{once:true})
+    genButton.addEventListener("click", show)
 }
