f5a78317d9b3cdff4f517bea62455a1347286284
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-10-18 18:32:01 -0500

    Update pattern.js

--- Excercise_19/pattern.js
+++ Excercise_19/pattern.js
@@ -4,37 +4,48 @@ btn.addEventListener("click",searchPattern)
 function searchPattern(){
     let p = document.getElementById("pattern").value
     let text = document.querySelector(".text").textContent
-    findMatchingText(text,p)
+    console.log(findMatchingText(text,p))
 }
 
 function findMatchingText(text, p){
     let matches = []
+    let newVal
     const firstString = p.replace(/\*/g,"")[0]
-    if(p[0] != firstString){
-        var newVal = p.indexOf(firstString)
+    if(p[0] != firstString && firstString != undefined){
+        newVal = p.indexOf(firstString)
     }else{
-        var newVal = 0
+        newVal = 0
     }
-    if(text.indexOf(firstString)< newVal  || text.indexOf(firstString)== -1){
-        return "there are no matches"
+    if(text.indexOf(firstString) == -1){
+        if(!firstString){
+            let size = p.length
+            let i = 0
+            let matches = []
+            do{
+                matches.push(text.slice(i, i + size))
+                i++
+            }while(i < text.length-size)
+            return matches
+        }else{
+            return null
+        }
     }else{
-        let count =1;
+        let count = 1;
         let word = p[newVal]
         let indices = [];
         for(let i=0; i<text.length;i++) {
             if (text[i] === p[newVal]) indices.push(i);
         }
-        
         indices.forEach(e => {
             let index = Number(e)
             for(let i=1; i<newVal+1; i++){
-                if(text[index-i]==p[newVal-i] || p[newVal-i]=="*"){
+                if(text[index-i]==p[newVal-i] || p[newVal-i]=="*" && text[index-i] != undefined){
                     count++
                     word = text[index-i] + word
                 }
             }
             for(let i=1; i<p.length-newVal; i++){
-                if(text[index+i]==p[newVal+i] || p[newVal+i]=="*"){
+                if(text[index+i]==p[newVal+i] || p[newVal+i]=="*" &&text[index+i] != undefined){
                     count++
                     word = word + text[index+i] 
                 }
@@ -49,5 +60,6 @@ function findMatchingText(text, p){
             }
         });
     }
-    return matches
+    if(matches == []) return null
+    else return matches
 }
