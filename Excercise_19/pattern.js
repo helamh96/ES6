839f081144f6e021a6a3c2217029f986d1eee94f
helamh96 <94868147+helamh96@users.noreply.github.com>
2023-06-14 14:43:48 -0600

    Update pattern.js

--- Excercise_19/pattern.js
+++ Excercise_19/pattern.js
@@ -1,69 +1,51 @@
-let btn = document.getElementById("btn")
-btn.addEventListener("click",searchPattern)
-
-function searchPattern(){
-    let p = document.getElementById("pattern").value
-    let text = document.querySelector(".text").textContent
-    console.log(findMatchingText(text,p))
+function compare(string, pattern){
+    let isSame = true
+    if(string.length!=pattern.length){
+        return false
+    }else{
+        for(let i=0; i<string.length; i++){
+            if(string[i]!==pattern[i]&&pattern[i]!=="*"){
+                isSame = false        
+            }
+        }
+    }
+    return isSame
 }
 
-function findMatchingText(text, p){
-    let matches = []
-    let newVal
-    const firstString = p.replace(/\*/g,"")[0]
-    if(p[0] != firstString && firstString != undefined){
-        newVal = p.indexOf(firstString)
-    }else{
-        newVal = 0
+function getAllIndex(string,char){
+    let indices = [];
+    for(let i=0; i<string.length;i++) {
+        if (string[i] === char) indices.push(i);
     }
-    if(text.indexOf(firstString) == -1){
-        if(!firstString){
-            let size = p.length
-            let i = 0
-            let matches = []
-            do{
-                if(text[i]!==undefined && text[i+size-1]!==undefined){
-                    matches.push(text.slice(i, i + size))
-                    break
-                }
-                i = i+size
-            }while(i < text.length-size+1)
-            return matches.length===0 ? null : matches
+    return indices;
+}
+
+function match(string, pattern){
+    let firstNonAstherisk = pattern.replaceAll("*","")
+    if(firstNonAstherisk[0]==undefined){
+        if(pattern.length<=string.length){
+            return string.slice(0, pattern.length)
         }else{
             return null
         }
     }else{
-        let word = p[newVal]
-        let indices = [];
-        for(let i=0; i<text.length;i++) {
-            if (text[i] === p[newVal]){
-                indices.push(i);
-            }
+        const indexOfFirstChar = string.indexOf(firstNonAstherisk[0])
+        if(indexOfFirstChar == -1){
+            return null
+        }else{
+            let matchingIndex = getAllIndex(string, firstNonAstherisk[0])
+            const matchingSubstring = matchingIndex.map(e => string.slice(e-pattern.indexOf(firstNonAstherisk[0]), -pattern.indexOf(firstNonAstherisk[0])+pattern.length+e))
+            let isFound = false
+            let match = null
+            matchingSubstring.forEach(el => {
+                if(!isFound){
+                    if(compare(el,pattern)){
+                        match = el
+                    }
+                }
+                
+            })
+            return match
         }
-        let count = 1;
-        indices.every(e => {
-            let index = Number(e)
-            for(let i=1; i<newVal+1; i++){
-                if(text[index-i]==p[newVal-i] || (p[newVal-i]=="*" && text[index-i] != undefined)){
-                    count++
-                    word = text[index-i] + word
-                }
-            }
-            for(let i=1; i<p.length-newVal; i++){
-                if(text[index+i]==p[newVal+i] || (p[newVal+i]=="*" && text[index+i] != undefined)){
-                    count++
-                    word = word + text[index+i] 
-                }
-            }
-            if(count === p.length){
-                matches.push(word)
-                return false;
-            }else{
-                word = p[newVal]
-                count = 1;
-                return true;
-            }
-        });
     }
-    return matches.length===0 ? null : matches
 }
